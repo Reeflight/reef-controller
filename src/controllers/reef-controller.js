a33d98f27dc6c860726d9a7fefbbdd65e1bdc36f
AndrewVanardennen <Andrew.vanardennen@gmail.com>
2017-02-17 18:00:36 +0100

    it just works

---
+++ src/controllers/reef-controller.js
@@ -0,0 +1,15 @@
+'use strict';
+import FirebaseController from './firebase-controller.js';
+
+export default class extends FirebaseController {
+  constructor() {
+    const i2c = require('i2c');
+    let address = '0x10'; // set address to 0x10
+    let wire = new i2c(address, {device: '/dev/i2c-1'});
+
+
+    wire.writeByte(1, err => {
+      console.log("error is ", err);
+    });
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,15 +1,4 @@
 'use strict';
-import FirebaseController from './controllers/firebase-controller.js';
+import ReefController from './controllers/reef-controller';
 
-export default class extends FirebaseController {
-  constructor() {
-    const i2c = require('i2c');
-    let address = '0x10'; // set address to 0x10
-    let wire = new i2c(address, {device: '/dev/i2c-1'});
-
-
-    wire.writeByte(1, err => {
-      console.log("error is ", err);
-    });
-  }
-};
+new ReefController();
