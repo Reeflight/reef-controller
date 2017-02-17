fc61823b28aa510914ca801618924d7e301e6717
AndrewVanardennen <Andrew.vanardennen@gmail.com>
2017-02-17 18:24:50 +0100

    :space_invader: update infrastructure

--- src/controllers/reef-controller.js
+++ src/controllers/reef-controller.js
@@ -3,10 +3,12 @@ import FirebaseController from './firebase-controller.js';
 
 export default class extends FirebaseController {
   constructor() {
+    this.start();
+  }
+  start() {
     const i2c = require('i2c');
     let address = 0x10; // set address to 0x10
     let wire = new i2c(address, {device: '/dev/i2c-1'});
-
 
     wire.writeByte(1, err => {
       console.log("error is ", err);
