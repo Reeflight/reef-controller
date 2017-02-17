4ea0bb90e3cfca85c3f2128ba0a45f0f4e16bbd4
AndrewVanardennen <Andrew.vanardennen@gmail.com>
2017-02-17 13:00:02 +0100

    test i2c

---
+++ src/index.js
@@ -0,0 +1,8 @@
+'use strict';
+const i2c = require('i2c');
+let address = 0x10; // set address to 0x10
+let wire = new i2c(address, {device, '/dev/i2c-1'});
+
+wire.writeByte(1, err => {
+  console.log("error is ", err);
+});
