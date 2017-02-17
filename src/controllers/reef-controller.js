ce1168a51dc051658888d749ba9720a372927745
AndrewVanardennen <Andrew.vanardennen@gmail.com>
2017-02-17 18:20:34 +0100

    :space_invader:  to integer and beyond

--- src/controllers/reef-controller.js
+++ src/controllers/reef-controller.js
@@ -4,7 +4,7 @@ import FirebaseController from './firebase-controller.js';
 export default class extends FirebaseController {
   constructor() {
     const i2c = require('i2c');
-    let address = '0x10'; // set address to 0x10
+    let address = 0x10; // set address to 0x10
     let wire = new i2c(address, {device: '/dev/i2c-1'});
 
 
