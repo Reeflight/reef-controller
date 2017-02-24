4a6531694e6758351eadcb92be9122167ebdb5d9
AndrewVanardennen <Andrew.vanardennen@gmail.com>
2017-02-24 23:09:20 +0100

    :dragon: add data-controller

--- src/controllers/reef-controller.js
+++ src/controllers/reef-controller.js
@@ -1,19 +1,7 @@
 'use strict';
-import FirebaseController from './firebase-controller.js';
-import I2cController from './i2c-controller.js';
-const i2c = new I2cController();
-export default class extends FirebaseController {
+import DataController from './data-controller.js'
+export default class extends DataController {
   constructor() {
     super();
-    this.start();
-  }
-  get firebase() {
-    return global.firebase;
-  }
-  start() {
-    this.firebase.database().ref('users/XpsE3FKDooeYDJwkxES9JLG8BPZ2/channel/').on('value', snapshot => {
-      const data = snapshot.val();
-      i2c.write(data);
-    });
   }
 };
